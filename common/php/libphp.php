c5c358b92cc069e6984f9c07b746f6d1a73568b9
Michel Dumontier <michel.dumontier@gmail.com>
2012-01-05 10:19:01 -0500

    fixed typo added recursive copy functions call to ncbo bioportal

--- common/php/libphp.php
+++ common/php/libphp.php
@@ -13,15 +13,17 @@ $gns = array(
   'foaf' => 'http://xmlns.com/foaf/0.1/',
   'sio' => 'http://semanticscience.org/resource/',
   'bio2rdf' => BIO2RDF_URI,
-  'bio2rdf_vocabulary' = > BIO2RDF_URI.'bio2rdf_vocabulary:'
+  'bio2rdf_vocabulary' => BIO2RDF_URI.'bio2rdf_vocabulary:'
 );
 
 // valid dataset namespaces
-$gdataset_ns = array('afcs', 'apo','bind','biogrid','blastprodom','candida','cas','chebi','ctd','dbsnp','dip','ddbj','drugbank','ec','embl','ensembl','eco','euroscraf','flybase','fprintscan','kegg','gene3d','germonline','go','gp','grid','hmmsmart','hmmpanther','hmmpfam','hmmpir','hmmtigr','iubmb','intact','ipi','irefindex','mesh','metacyc','mi','mint','mips','geneid','refseq','omim','ophid','patternscan','pato','pharmgkb','pir','prf','profilescan','pdb','pubmed','pubchem','seg','sgd','so','superfamily','swissprot','taxon','tcdb','tigr','tpg','trembl','uniparc','uniprot','uo','registry','registry_dataset');
+$gdataset_ns = array('afcs', 'apo','bind','biogrid','blastprodom','candida','cas','chebi','coil','ctd','dbsnp','dip','ddbj','drugbank','ec','embl','ensembl','eco','euroscraf','flybase','fprintscan','kegg','gene3d','germonline','go','gp','grid','hmmsmart','hmmpanther','hmmpfam','hmmpir','hmmtigr','iubmb','intact','ipi','irefindex','mesh','metacyc','mi','mint','mips','geneid','refseq','omim','ophid','patternscan','pato','pharmgkb','pir','prf','profilescan','pdb','pubmed','pubchem','seg','sgd','so','superfamily','swissprot','taxon','tcdb','tigr','tpg','trembl','uniparc','uniprot','uo','registry','registry_dataset');
 	
 // add the valid namespaces to the global namespace array
 foreach($gdataset_ns AS $ns) {
   $gns[$ns] = BIO2RDF_URI.$ns.':';
+  $gns[$ns.'_vocabulary'] = BIO2RDF_URI.$ns.'_vocabulary:';
+  $gns[$ns.'_resource'] = BIO2RDF_URI.$ns.'_resource:';
 }
 
 /** Generate the N3 prefix header **/
@@ -38,12 +40,14 @@ function N3NSHeader()
 /** to download files */
 function DownloadFiles($host, $files, $ldir)
 {
- foreach($files AS $file) {
+ foreach($files AS $filepath) {
   echo 'creating local download path '.$ldir.PHP_EOL;
   @mkdir($ldir,null,true);
   
-  echo "Downloading $host$file ... ";
-  if(!copy($host.$file,$ldir.$file)) {
+  BreakPath($filepath, $dir, $file);
+  
+  echo "Downloading $host$dir$file ... ";
+  if(!copy($host.$dir.$file,$ldir.$file)) {
     $errors= error_get_last();
     echo $errors['type']." : ".$errors['message'].PHP_EOL;
   } else {
@@ -64,6 +68,75 @@ function BreakPath($path, &$dir, &$file)
 	$file = $path;
    }
    return 0;
+}
+
+
+/**
+ * Copy a file, or recursively copy a folder and its contents
+ *
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     1.0.1
+ * @link        http://aidanlister.com/repos/v/function.copyr.php
+ * @param       string   $source    Source path
+ * @param       string   $dest      Destination path
+ * @return      bool     Returns TRUE on success, FALSE on failure
+ */
+function copyr($source, $dest)
+{
+    // Check for symlinks
+    if (is_link($source)) {
+        return symlink(readlink($source), $dest);
+    }
+
+    // Simple copy for a file
+    if (is_file($source)) {
+        return copy($source, $dest);
+    }
+
+    // Make destination directory
+    if (!is_dir($dest)) {
+        mkdir($dest);
+    }
+
+    // Loop through the folder
+    $dir = dir($source);
+    while (false !== $entry = $dir->read()) {
+        // Skip pointers
+        if ($entry == '.' || $entry == '..') {
+            continue;
+        }
+
+        // Deep copy directories
+        copyr("$source/$entry", "$dest/$entry");
+    }
+
+    // Clean up
+    $dir->close();
+    return true;
+}
+
+function GetDirFiles($dir,$pattern)
+{
+ if(!is_dir($dir)) {
+  echo "$dir not a directory".PHP_EOL;
+  return 1;
+ }
+
+ $dh = opendir($dir);
+ while (($file = readdir($dh)) !== false) {
+  if($file == '.' || $file == '..') continue;
+  $files[] = $file;
+ }
+ sort($files);
+ closedir($dh);
+ return $files; 
+}
+
+function GetLatestNCBOOntology($ontology_id,$apikey,$target_filepath)
+{
+  @mkdir($dl_dir,null,true);
+  file_put_contents($target_filepath, 
+     file_get_contents('http://rest.bioontology.org/bioportal/virtual/download/'.$ontology_id.'?apikey='.$apikey));
 }
 
 
