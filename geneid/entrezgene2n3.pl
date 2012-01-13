2b108a8a38a6a2141139da29538fa51fd661efed
Alison Callahan <acallaha@connect.carleton.ca>
2012-01-13 15:05:40 -0500

    added copyright notices

--- geneid/entrezgene2n3.pl
+++ geneid/entrezgene2n3.pl
@@ -1,3 +1,25 @@
+###############################################################################
+#Copyright (C) 2011 Alison Callahan, Marc-Alexandre Nolin
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in
+#the Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#of the Software, and to permit persons to whom the Software is furnished to do
+#so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+###############################################################################
+
 #!/usr/bin/perl 
 
 use strict;
@@ -16,9 +38,6 @@ use Digest::MD5;
 #
 # The structure of Entrez Gene ASN.1 file can be found here [http://www.ncbi.nlm.nih.gov/IEB/ToolBox/CPP_DOC/asn_spec/entrezgene.asn.html]
 #
-# Copyright: (c) May 2010,Marc-Alexandre Nolin - manolin AT gmail.com, Laval University; December 2011 Alison Callahan Carleton University
-# Reviewed by:
-# License: this code is licensed under Perl itself or GPL.
 # Base on the Bio::ASN1::EntrezGene parser of Mingyi Liu (c) 2005 [http://www.ncbi.nlm.nih.gov/pubmed/15879451]
 # URI: <http://bio2rdf.org/pubmed:15879451>
 #
