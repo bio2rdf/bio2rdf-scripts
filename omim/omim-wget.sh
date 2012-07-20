f2778ad7192146936e8cc00b9f2a017045e6eeea
Michel Dumontier <michel.dumontier@gmail.com>
2012-07-20 15:29:39 -0400

    removed legacy perl scripts

--- omim/omim-wget.sh
+++
@@ -1,9 +0,0 @@
-# omim-wget.sh
-# ftp://ftp.ncbi.nih.gov/repository/OMIM/
-
-cd /bio2rdf/data/omim
-rm -rf /bio2rdf/data/omim/*
-
-wget ftp://ftp.ncbi.nih.gov/repository/OMIM/*  --output-file=omim-wget.log
-
-gunzip omim.txt.Z
