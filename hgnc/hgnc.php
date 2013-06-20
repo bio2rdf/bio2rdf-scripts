d2fbec5f51210f6d05a111f48a92df36a93360d9
Martijn van Iersel <martijn@generalbioinformatics.com>
2013-06-20 12:29:40 +0100

    HGNC: fix column numbering. fix escaping quotes for locus specific databases.

--- hgnc/hgnc.php
+++ hgnc/hgnc.php
@@ -160,17 +160,16 @@ class HGNCParser extends RDFFactory {
 			$primary_ids = $fields[27];
 			$secondary_ids = $fields [28];
 			$ccd_ids = $fields[29];
-			$vega_ids = $fields[23];
+			$vega_ids = $fields[30];
 			$locus_specific_databases = $fields[31];
-			$gdb_id_mappeddata = $fields[32];
-			$entrez_gene_id_mappeddatasuppliedbyNCBI = $fields[33];
-			$omim_id_mappeddatasuppliedbyNCBI = $fields[34];
-			$refseq_mappeddatasuppliedbyNCBI = $fields[35];
-			$uniprot_id_mappeddatasuppliedbyUniProt = $fields[36];
-			$ensembl_id_mappeddatasuppliedbyEnsembl = $fields[37];
-			$ucsc_id_mappeddatasuppliedbyUCSC = $fields[38];
-			$mouse_genome_database_id_mappeddatasuppliedbyMGI = $fields[39];
-			$rat_genome_database_id_mappeddatasuppliedbyRGD = $fields[40];
+			$entrez_gene_id_mappeddatasuppliedbyNCBI = $fields[32];
+			$omim_id_mappeddatasuppliedbyNCBI = $fields[33];
+			$refseq_mappeddatasuppliedbyNCBI = $fields[34];
+			$uniprot_id_mappeddatasuppliedbyUniProt = $fields[35];
+			$ensembl_id_mappeddatasuppliedbyEnsembl = $fields[36];
+			$ucsc_id_mappeddatasuppliedbyUCSC = $fields[37];
+			$mouse_genome_database_id_mappeddatasuppliedbyMGI = $fields[38];
+			$rat_genome_database_id_mappeddatasuppliedbyRGD = $fields[39];
 
 			$this->AddRDF($this->QQuad($id, "rdf:type", "hgnc_vocabulary:GeneSymbol"));
 			$this->AddRDF($this->QQuad($id, "void:inDataset", $this->GetDatasetURI()));
@@ -341,7 +340,7 @@ class HGNCParser extends RDFFactory {
 			}
 
 			if(!empty($locus_specific_databases)){
-				$this->AddRDF($this->QQuadL($id, "hgnc_vocabulary:locus_specific_databases", "$locus_specific_databases"));
+				$this->AddRDF($this->QQuadL($id, "hgnc_vocabulary:locus_specific_databases", $this->SafeLiteral($locus_specific_databases)));
 			}
 
 			/*if(!empty($gdb_id_mappeddata)){
