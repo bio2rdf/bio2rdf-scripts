6d9eddb201418c1987edd18c76bace9096d52d07
Jose Cruz-Toledo <josemiguelcruztoledo@gmail.com>
2013-07-28 16:50:59 -0400

    finished updating process()

--- iproclass/iproclass.php
+++ iproclass/iproclass.php
@@ -28,39 +28,22 @@ SOFTWARE.
  * @author Jose Cruz-Toledo
 */
 
-require('../../php-lib/rdfapi.php');
+require(__DIR__.'/../../php-lib/bio2rdfapi.php');
 
-class IProClassParser extends RDFFactory{
+class IProClassParser extends Bio2RDFizer{
 
-	private $version = null;
+	private $version = 2.0;
 
-	function __construct($argv) {
-		parent::__construct();
-		$this->SetDefaultNamespace("iproclass");
-		
-		// set and print application parameters
-		$this->AddParameter('indir',false,null,'/data/download/iproclass/','directory to download into and parse from');
-		$this->AddParameter('outdir',false,null,'/data/rdf/iproclass/','directory to place rdfized files');
-		$this->AddParameter('graph_uri',false,null,null,'provide the graph uri to generate n-quads instead of n-triples');
-		$this->AddParameter('gzip',false,'true|false','true','gzip the output');
-		$this->AddParameter('download',false,'true|false','false','set true to download files');
-		$this->AddParameter('download_url',false,null,'ftp://ftp.pir.georgetown.edu/databases/iproclass/');
-		if($this->SetParameters($argv) == FALSE) {
-			$this->PrintParameters($argv);
-			exit;
-		}
-		
-		if($this->CreateDirectory($this->GetParameterValue('indir')) === FALSE) exit;
-		if($this->CreateDirectory($this->GetParameterValue('outdir')) === FALSE) exit;
-		if($this->GetParameterValue('graph_uri')) $this->SetGraphURI($this->GetParameterValue('graph_uri'));
-		
-		return TRUE;
+	public function __construct($argv) {
+		parent::__construct($argv, "iproclass");
+		parent::addParameter('files',true,'all','all','files to process');
+		parent::addParameter('download',false,'true|false','false','set true to download files');
+		parent::addParameter('download_url', false, null,'ftp://ftp.pir.georgetown.edu/databases/iproclass/');
+		parent::initialize();
 	}
 
-	function Run(){
-
+	public function Run(){
 		$file = "iproclass.tb.gz";
-
 		$ldir = $this->GetParameterValue('indir');
 		$odir = $this->GetParameterValue('outdir');
 		$rdir = $this->GetParameterValue('download_url');
@@ -69,19 +52,16 @@ class IProClassParser extends RDFFactory{
 		if(substr($ldir, -1) !== "/"){
 			$ldir = $ldir."/";
 		}
-		
 		if(substr($odir, -1) !== "/"){
 			$odir = $odir."/";
 		}
-		
 		$lfile = $ldir.$file;
-
 		if(!file_exists($lfile) && $this->GetParameterValue('download') == false) {
-			trigger_error($lfile." not found. Will attempt to download.", E_USER_NOTICE);
-			$this->SetParameterValue('download',true);
+			trigger_error($file." not found. Will attempt to download.", E_USER_NOTICE);
+			parent::setParameterValue('download',true);
 		}
 		
-		//download all files [except mapping file]
+		//download all files 
 		if($this->GetParameterValue('download') == true) {
 			$rfile = $rdir.$file;
 			echo "downloading $file... ";
@@ -90,44 +70,39 @@ class IProClassParser extends RDFFactory{
 
 		$ofile = $odir.'iproclass.nt'; 
 		$gz = false;
-		if($this->GetParameterValue('graph_uri')){$ofile = $odir.'iproclass.nq'; }
-		if($this->GetParameterValue('gzip')) {
+		if(strstr(parent::getParameterValue('output_format'), "gz")) {
 			$ofile .= '.gz';
 			$gz = true;
 		}
-		
-		$this->SetReadFile($lfile, true);
-		$this->SetWriteFile($ofile, $gz);
-
+		parent::setReadFile($lfile);
+		parent::setWriteFile($ofile, $gz);
 		echo "processing $file... ";
 		$this->process();
-
+		echo "done!".PHP_EOL;
 		//close write file
-		$this->GetWriteFile()->Close();
+		parent::getWriteFile()->close();
 		
 		echo "generating dataset release file... ";
-		$desc = $this->GetBio2RDFDatasetDescription(
-			$this->GetNamespace(),
+		$desc = parent::getBio2RDFDatasetDescription(
+			$this->getPrefix(),
 			"https://github.com/bio2rdf/bio2rdf-scripts/blob/master/iproclass/iproclass.php", 
-			$this->GetBio2RDFDownloadURL($this->GetNamespace()),
-			"http://pir.georgetown.edu/iproclass/",
+			$this->getBio2RDFDownloadURL($this->getNamespace()),
+			"http://pir.georgetown.edu/iproclass",
 			array("restricted-by-source-license"),
 			"http://pir.georgetown.edu/pirwww/about/linkpir.shtml",
-			$this->GetParameterValue('download_url'),
-			$this->version
+			parent::getParameterValue('download_url'),
+			parent::getDatasetVersion()
 		);
-		$this->SetWriteFile($odir.$this->GetBio2RDFReleaseFile($this->GetNamespace()));
-		$this->GetWriteFile()->Write($desc);
-		$this->GetWriteFile()->Close();
-		
+		parent::setWriteFile($odir.$this->getBio2RDFReleaseFile($this->GetNamespace()));
+		parent::getWriteFile()->write($desc);
+		parent::getWriteFile()->close();
 
 		echo "done!".PHP_EOL;
 
 	}//Run
 
-	function process(){
-
-		while($l = $this->GetReadFile()->Read(4096)) {
+	private function process(){
+		while($l = $this->GetReadFile()->Read(200000)) {
 			$fields = explode("\t", $l);
 			@$uniprot_acc = $fields[0];
 			@$uniprot = $fields[1];
@@ -153,46 +128,60 @@ class IProClassParser extends RDFFactory{
 			@$embl_protein = trim($fields[21]);
 
 			$id = "uniprot:".$uniprot_acc;
+			$id_res = $this->getNamespace().$id;
+			$id_label = "uniprot accession";
 
 			if(!empty($uniprot)){
 				$uniprot_ids = explode("; ", $uniprot);
 				foreach($uniprot_ids as $uniprot_id){
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-uniprot", "uniprot:".$uniprot_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-uniprot", "uniprot:".$uniprot_id)
+					);
 				}
 			}
 			
 			if(!empty($gene)){
 				$gene_ids = explode("; ", $gene);
 				foreach($gene_ids as $gene_id){
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-geneid", "geneid:".$gene_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-ncbigene", "geneid:".$gene_id)
+					);
 				}	
 			}
 			
 			if(!empty($refseq)){
 				$refseq_ids = explode("; ", $refseq);
 				foreach ($refseq_ids as $refseq_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-refseq", "refseq:".$refseq_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-refseq", "refseq:".$refseq_id)
+					);
 				}	
 			}			
 
 			if(!empty($gi)){
 				$gi_ids = explode("; ", $gi);
 				foreach ($gi_ids as $gi_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-gi", "gi:".$gi_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-gi", "gi:".$gi_id)
+					);
 				}	
 			}
 			
 			if(!empty($pdb)){
 				$pdb_ids = explode("; ", $pdb);
 				foreach ($pdb_ids as $pdb_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-pdb", "pdb:".$pdb_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-pdb", "pdb:".$pdb_id)
+					);
 				}
 			}
 
 			if(!empty($pfam)){
 				$pfam_ids = explode("; ", $pfam);
 				foreach ($pfam_ids as $pfam_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-pfam", "pfam:".$pfam_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-pfam", "pfam:".$pfam_id)
+					);
 				}
 			}
 
@@ -200,99 +189,127 @@ class IProClassParser extends RDFFactory{
 				$go_ids = explode("; ", $go);
 				foreach ($go_ids as $go_id) {
 					$go_id = substr($go_id, 3);
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-go", "go:".$go_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-go", "go:".$go_id)
+					);
 				}
 			}
 
 			if(!empty($pirsf)){
 				$pirsf_ids = explode("; ", $pirsf);
 				foreach ($pirsf_ids as $pirsf_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-pirsf", "pirsf:".$pirsf_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-pirsf", "pirsf:".$pirsf_id)
+					);
 				}
 			}
 
 			if(!empty($ipi)){
 				$ipi_ids = explode("; ", $ipi);
 				foreach ($ipi_ids as $ipi_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-ipi", "ipi:".$ipi_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-ipi", "ipi:".$ipi_id)
+					);
 				}
 			}
 
 			if(!empty($uniref_100)){
 				$uniref_100_ids = explode("; ", $uniref_100);
 				foreach ($uniref_100_ids as $uniref_100_id) {
-					$this->AddRDF($this->QQuadO_URL($id, "rdfs:seeAlso", "http://uniprot.org/uniref/".$uniref_100_id));
+					parent::AddRDF(
+						parent::QQuaadO_URL($id_res, "rdfs:seeAlso", "http://uniprot.org/uniref/".$uniref_100_id)
+					);
 				}
 			}
 
 			if(!empty($uniref_90)){
 				$uniref_90_ids = explode("; ", $uniref_90);
 				foreach ($uniref_90_ids as $uniref_90_id) {
-					$this->AddRDF($this->QQuadO_URL($id, "rdfs:seeAlso", "http://uniprot.org/uniref/".$uniref_90_id));
+					parent::AddRDF(
+						parent::QQuaadO_URL($id_res, "rdfs:seeAlso", "http://uniprot.org/uniref/".$uniref_90_id)
+					);
 				}
 			}
 
 			if(!empty($uniref_50)){
 				$uniref_50_ids = explode("; ", $uniref_50);
 				foreach ($uniref_50_ids as $uniref_50_id) {
-					$this->AddRDF($this->QQuadO_URL($id, "rdfs:seeAlso", "http://uniprot.org/uniref/".$uniref_50_id));
+					parent::AddRDF(
+						parent::QQuaadO_URL($id_res, "rdfs:seeAlso", "http://uniprot.org/uniref/".$uniref_50_id)
+					);
 				}
 			}
 
 			if(!empty($uniparc)){
 				$uniparc_ids = explode("; ", $uniparc);
 				foreach ($uniparc_ids as $uniparc_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-uniparc", "uniparc:".$uniparc_id));
-					$this->AddRDF($this->QQuadO_URL($id, "rdfs:seeAlso", "http://uniprot.org/uniparc/".$uniparc_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-uniparc", "uniparc:".$uniparc_id).
+						parent::QQuaadO_URL($id_res, "rdfs:seeAlso", "http://uniprot.org/uniparc/".$uniparc_id)
+					);
 				}
 			}
 
 			if(!empty($ncbi_taxonomy)){
 				$taxonomy_ids = explode("; ", $ncbi_taxonomy);
 				foreach ($taxonomy_ids as $taxonomy_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-taxon", "taxon:".$taxonomy_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-taxon", "taxon:".$taxonomy_id)
+					);
 				}
 			}
 
 			if(!empty($mim)){
 				$mim_ids = explode("; ", $mim);
 				foreach($mim_ids as $mim_id){
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-omim", "omim:".$mim_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-omim", "omim:".$mim_id)
+					);
 				}
 			}
 
 			if(!empty($unigene)){
 				$unigene_ids = explode("; ", $unigene);
 				foreach ($unigene_ids as $unigene_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-unigene", "unigene:".$unigene_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-unigene", "unigene:".$unigene_id)
+					);
 				}
 			}
 
 			if(!empty($ensembl)){
 				$ensembl_ids = explode("; ", $ensembl);
 				foreach ($ensembl_ids as $ensembl_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-ensembl", "ensembl:".$ensembl_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-ensembl", "ensembl:".$ensembl_id)
+					);
 				}
 			}
 
 			if(!empty($pubmed)){
 				$pubmed_ids = explode("; ", $pubmed);
 				foreach ($pubmed_ids as $pubmed_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-pubmed", "pubmed:".$pubmed_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-pubmed", "pubmed:".$pubmed_id)
+					);
 				}
 			}
 
 			if(!empty($embl_genbank_ddbj)){
 				$genbank_ids = explode("; ", $embl_genbank_ddbj);
 				foreach ($genbank_ids as $genbank_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-genbank", "genbank:".$genbank_id));
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-genbank", "genbank:".$genbank_id)
+					);
 				}
 			}
 
 			if(!empty($embl_protein)){
 				$embl_protein_ids = explode(";", $embl_protein);
 				foreach ($embl_protein_ids as $embl_protein_id) {
-					$this->AddRDF($this->QQuad($id, "iproclass_vocabulary:x-genbank", "genbank:".$embl_protein_id));//not an error! these are in fact genbank identifiers
+					parent::AddRDF(
+						parent::triplify($id_res, $this->getVoc()."x-genbank", "genbank:".$embl_protein_id)
+					);
 				}
 			}
 
@@ -300,6 +317,7 @@ class IProClassParser extends RDFFactory{
 			$this->WriteRDFBufferToWriteFile();
 
 		}//while
+		
 	}
 }
 $start = microtime(true);
