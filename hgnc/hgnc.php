b62fe0922668f2e2028d0621779ae6e2acf0f854
Jose Cruz-Toledo <josemiguelcruztoledo@gmail.com>
2013-08-07 20:54:02 -0400

    fixed typo and triple

--- hgnc/hgnc.php
+++ hgnc/hgnc.php
@@ -181,8 +181,7 @@ class HGNCParser extends Bio2RDFizer {
 			$id_label = "Gene Symbol for ".$approved_symbol;
 			$id_label_class = "hgnc identifier: ".$id;
 			parent::AddRDF(
-				parent::triplify($id_res, "rdf:type", $this->getVoc()."Gene-Symbol").
-				parent::describeIndividual($id_res, $id_label, $this->getVoc()."gene symbol").
+				parent::describeIndividual($id_res, $id_label, $this->getVoc()."Gene-Symbol").
 				parent::describeClass($this->getVoc()."Gene-Symbol", $id_label_class)
 			);
 			if(!empty($approved_symbol)){
