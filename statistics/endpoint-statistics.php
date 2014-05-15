9571a08a04da559724bd6f31d774ffffcd7f918c
Michel Dumontier <michel.dumontier@gmail.com>
2014-05-14 22:12:36 -0700

    added uuid

--- statistics/endpoint-statistics.php
+++ statistics/endpoint-statistics.php
@@ -215,7 +215,8 @@ function query($sparql)
 }
 function getID($obj)
 {
-	return "http://bio2rdf.org/bio2rdf.dataset_resource:".uniqid().md5(json_encode($obj));
+	$v4uuid = UUID::v4();
+	return "http://bio2rdf.org/bio2rdf.dataset_resource:".$v4uuid;
 }
 function Quad($subject_uri, $predicate_uri, $object_uri, $graph_uri = null)
 {
@@ -671,5 +672,30 @@ function addDatasetPropertyDatasetCount()
 	}
 }
 
+
+class UUID {
+  public static function v4() {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+      // 32 bits for "time_low"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+      // 16 bits for "time_mid"
+      mt_rand(0, 0xffff),
+
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 4
+      mt_rand(0, 0x0fff) | 0x4000,
+
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      mt_rand(0, 0x3fff) | 0x8000,
+
+      // 48 bits for "node"
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+  }
+}
 
 ?>
