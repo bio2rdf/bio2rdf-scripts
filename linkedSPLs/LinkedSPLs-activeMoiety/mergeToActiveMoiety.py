86897e6ca0769987aba5fc8f6e51a45dea90211c
yin2 <yin2@yin2.(none)>
2014-08-18 11:44:59 -0400

    merge files to create a csv for all data required in active moiety sub graph

--- linkedSPLs/LinkedSPLs-activeMoiety/mergeToActiveMoiety.py
+++ linkedSPLs/LinkedSPLs-activeMoiety/mergeToActiveMoiety.py
@@ -9,10 +9,11 @@ Created 08/15/2014
 
 import os, sys
 import pandas as pd
+from StringIO import StringIO
 
 ## Define data inputs
 
-DRON_CHEBI = "dronid_chebi_rxcui-07232014.txt"
+DRON_CHEBI_RXCUI = "dronid_chebi_rxcui-07232014.txt"
 OMOP_RXCUI = "omopid_rxcui.csv"
 PT_CHEBI = "pt_chebi-03132014.txt"
 PT_DRUGBANK = "pt_drugbank-04082014.txt"
@@ -27,7 +28,7 @@ CHEBI_BASE_URI = "http://purl.obolibrary.org/obo/"
 ## read mappings of pt and unii
 
 pt_unii_cols = ['id','pt','unii']
-pt_unii_DF = pd.read_csv(PT_UNII, sep='|', names=pt_unii_cols)
+pt_unii_DF = pd.read_csv(PT_UNII, sep='|', names=pt_unii_cols)[['pt','unii']]
 #print pt_unii_DF
 #pt_unii_DF.to_csv('test.csv', sep='|')
 
@@ -47,33 +48,61 @@ pt_rxcui_DF = pd.read_csv(PT_RXCUI, sep='\t', names=pt_rxcui_cols)
 ## read mappings of omop id and rxcui
 omop_rxcui_cols = ['omopid','rxcui']
 omop_rxcui_DF = pd.read_csv(OMOP_RXCUI, sep='|', names=omop_rxcui_cols)
-#print omop_rxcui_DF.head()
+#print omop_rxcui_DF.info()
 
 
 ## read mappings of pt and chebi
 
-chebi_rxcui_cols = ['pt','chebi']
-omop_rxcui_DF = pd.read_csv(PT_CHEBI, sep='\t', names=omop_rxcui_cols)
-print omop_rxcui_DF.head()
+pt_chebi_cols = ['pt','chebi']
+pt_chebi_DF = pd.read_csv(PT_CHEBI, sep='\t', names=pt_chebi_cols)
+#print pt_chebi_DF.info()
 
+
+## read mappings of dron and rxcui
+
+dron_chebi_rxcui_cols = ['dron','chebi','rxcui']
+dron_chebi_rxcui_DF = pd.read_csv(DRON_CHEBI_RXCUI, sep='|', names=dron_chebi_rxcui_cols)[['dron','rxcui']]
+#print dron_chebi_rxcui_DF
+
+
+## read mappings of unii, nui and preferredNameAndRole
+unii_nui_namerole_cols = ['setid', 'unii','nui','nameAndRole']
+unii_nui_namerole_DF = pd.read_csv(UNII_NUI_PREFERRED_NAME_ROLE, sep='\t', names=unii_nui_namerole_cols)[['unii','nui','nameAndRole']]
+print unii_nui_namerole_DF.info()
+#print unii_nui_namerole_DF.to_string()
 
 
 ## merge pt, unii and drugbank uri
 
 pt_unii_db_DF = pt_unii_DF.merge(pt_drugbank_DF, on=['pt'], how='left')
-print pt_unii_db_DF.info()
+#print pt_unii_db_DF.info()
 
 
 ## merge pt, unii, drugbank uri, rxcui together
 
 pt_unii_db_rxcui_DF = pt_unii_db_DF.merge(pt_rxcui_DF, on=['pt'], how='left')
-print pt_unii_db_rxcui_DF
-#print pt_unii_db_rxcui_DF.to_string()
-#print pt_unii_db_rxcui_DF.head()
-#pt_unii_db_rxcui_DF.to_csv('test.csv', sep='|')
+#print pt_unii_db_rxcui_DF
 
 
 ## merge omop to pt_unii_db_rxcui_DF
 
-pt_unii_db_rxcui_omop_DF = pt_unii_db_rxcui_DF.merge(omop_rxcui_DF, on=['rxcui'], how='left')
-print pt_unii_db_rxcui_omop_DF.info()
+merged_omop_DF = pt_unii_db_rxcui_DF.merge(omop_rxcui_DF, on=['rxcui'], how='left')
+#print merged_omop_DF.info()
+
+
+## merge chebi to merged_omop_DF
+
+merged_chebi_DF = merged_omop_DF.merge(pt_chebi_DF, on=['pt'], how='left')
+#print merged_chebi_DF
+
+
+## merge dronid to merged_chebi_DF
+merged_dron_DF = merged_chebi_DF.merge(dron_chebi_rxcui_DF, on=['rxcui'], how='left')
+print merged_dron_DF
+#print merged_dron_DF.to_string()
+
+## merge <nui> and <preferred name and role> to merged_dron_DF
+
+merged_epc_DF = merged_dron_DF.merge(unii_nui_namerole_DF, on=['unii'], how='left')
+print merged_epc_DF
+merged_epc_DF.to_csv('mergedActiveMoiety.csv', sep='|')
