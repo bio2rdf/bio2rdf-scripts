c6a44e264b9a990d494850bf4b74b33a55acc9dd
root <ningyifan1212@163.com>
2014-11-21 10:32:56 -0500

    clean linkedSPLs-upgrade

--- linkedSPLs/linkedSPLs-upgrade/mappings/DrOn-to-RxNorm/parseDrOnFromOWL.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rdfproc -c dron query sparql - '       
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX dron: <http://purl.obolibrary.org/obo/>  SELECT * WHERE {   ?dron dron:DRON_00010000 ?rxcui. }' > dron-to-chebi-and-rxnorm.txt
