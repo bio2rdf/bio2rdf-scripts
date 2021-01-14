6d4a3d02f133791e42b2361942cf7fe8ab0b6b82
Michel Dumontier <michel.dumontier@gmail.com>
2021-01-14 18:23:33 +0100

    Update mgi.php
    changed separator to vertical bar for genopheno data

--- mgi/mgi.php
+++ mgi/mgi.php
@@ -266,14 +266,14 @@ class MGIParser extends Bio2RDFizer
 			}
 
 			if($a[5]) {
-				$pmids = explode(",",$a[5]);
+				$pmids = explode("|",$a[5]);
 				foreach($pmids AS $pmid) {
 					parent::addRDF(		
 						parent::triplify($id,$this->getVoc()."x-pubmed","pubmed:".$pmid)	
 					);
 				}
 			}
-			$b = explode(",",$a[6]);
+			$b = explode("|",$a[6]);
 			foreach($b AS $marker) {
 				parent::addRDF(
 					parent::triplify($id,$this->getVoc()."marker",$marker).
